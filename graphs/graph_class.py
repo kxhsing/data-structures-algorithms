12057c8e11130c663b52ee167b35806d9e635a05
vagrant <kxhsing@gmail.com>
2017-07-25 19:24:47 +0000

    Fixed connection methods, working on paths

--- graphs/graph_class.py
+++ graphs/graph_class.py
@@ -1,4 +1,4 @@
-def Person(object):
+class Person(object):
     def __init__(self, name, adjacent=[]):
         """Create a person"""
 
@@ -9,9 +9,12 @@ def Person(object):
         return "<Person: {}>".format(self.name)
 
 
-def FriendGraph(object):
-    def __init__(self, name):
+class FriendGraph(object):
+    def __init__(self):
         self.people = {}
+
+    def __repr__(self):
+        return "<Friendship Graph>"
 
     def add_person(self, name):
         """Add a person to the graph"""
@@ -19,23 +22,28 @@ def FriendGraph(object):
         if name not in self.people:
             self.people[name] = Person(name)
 
+    def add_people(self, people_lst):
+        """Add a list of people to the graph"""
+
+        for person in people_lst:
+            self.add_person(person)
+
+    def get_people(self):
+        if self.people:
+            return self.people.keys()
+
     def make_friends(self, name, friend_names):
         """Make two people friends by adding each other to adjaceny lists"""
 
-        person = self.nodes[name]
+        person = self.people[name]
         for friend_name in friend_names:
             # for each friend in friend names, find the friend in the 
             # graph and add each other to each other's adjacency lists
-            friend = self.nodes[friend_name]
+            friend = self.people[friend_name]
 
             person.adjacent.add(friend)
             friend.adjacent.add(person)
 
-    def add_people(self, people_lst):
-        """Add a list of people to the graph"""
-
-        for person in person_lst:
-            self.people.add_person(person)
 
     def are_connected(self, name1, name2):
         """Are these two people connected in this graph"""
@@ -49,7 +57,7 @@ def FriendGraph(object):
 
             seen.add(node)
 
-            for friend in node.adjacency:
+            for friend in node.adjacent:
                 if friend in seen:
                     continue #don't finish loop, check next person
                 
@@ -68,12 +76,12 @@ def FriendGraph(object):
         if person1 == person2:
             return path
 
-        if not self.people[person1]:
+        if person1 not in self.people:
             return None
 
-        for friend in self.people[person1]:
+        for friend in self.people[person1].adjacent:
             if friend not in path:
-                newpath = find_path(friend, person2, path)
+                newpath = self.find_path(friend, person2, path)
                 if newpath:
                     return newpath
 
@@ -86,13 +94,13 @@ def FriendGraph(object):
         if person1 == person2:
             return [path]
 
-        if not self.people[person1]:
+        if person1 not in self.people:
             return []
 
         paths = []
 
-        for friend in self.people[person1]:
-            newpaths = find_all_paths(friend, person2, path)
+        for friend in self.people[person1].adjacent:
+            newpaths = self.find_all_paths(friend, person2, path)
             for newpath in newpaths:
                 paths.append(newpath)
 
@@ -106,19 +114,36 @@ def FriendGraph(object):
         if person1 == person2:
             return [path]
 
-        if not self.people[person1]:
+        if person1 not in self.people:
             return None
 
         shortest = None
 
-        for friend in self.people[person1]:
+        for friend in self.people[person1].adjacent:
             if friend not in path:
-                newpath = find_shortest_path(friend, person2, path)
+                newpath = self.find_shortest_path(friend, person2, path)
                 if newpath:
                     if not shortest or len(newpath) < len(shortest):
                         shortest = newpath
 
         return shortest
+
+
+
+friendship = FriendGraph()
+friendship.add_people(["apple", "berry", "cherry", "elderberry", "onion"])
+friendship.make_friends("apple", ["berry"])
+friendship.add_person("asparagus")
+friendship.add_people(["celery", "parsnip", "avocado"])
+friendship.add_person("tomato")
+friendship.make_friends("tomato", ["avocado"])
+friendship.make_friends("avocado", ["onion"])
+friendship.make_friends("onion", ["berry", "parsnip"])
+
+# friendship.find_all_paths("tomato", "onion")
+friendship.find_path("tomato", "onion")
+
+
         
 
 
